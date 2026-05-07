9432362c83e70d5ee1d73991ca954b1670881fd1
chaokaitang <1612993394@qq.com>
2026-05-07 13:14:44 +0800

    Refactor code structure for improved readability and maintainability

---
+++ method/rmsnorm.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-8):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+    def _norm(self, x: torch.Tensor):
+        return torch.rsqrt(x.pow(2).mean(-1,keepdim=True) + self.eps)
+    def forward(self, x: torch.Tensor):
+        return x * self._norm(x.float()).type_as(x) * self.weight
+    
