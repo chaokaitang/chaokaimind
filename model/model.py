6ec1f8bcd7145d662614b7d0f7fb9b12e17fcffb
chaokaitang <1612993394@qq.com>
2026-05-10 17:31:42 +0800

    fix bugs in attention module and add ffn

--- model/model.py
+++ model/model.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from transformers import PretrainedConfig
 import torch.nn.functional as F
+from transformers.activations import ACT2FN
 
 
 class ChaokaiMindConfig(PretrainedConfig):
@@ -109,7 +110,7 @@ class Attention(torch.nn.Module):
         self.num_key_value_heads = config.num_key_value_heads
         self.head_dim = config.head_dim
         self.hidden_size = config.hidden_size
-        self.is_casual = True
+        self.is_causal = True
         self.num_kv_repeats = self.num_attention_heads // self.num_key_value_heads
         self.q_proj = nn.Linear(self.hidden_size, self.num_attention_heads * self.head_dim, bias=False)
         self.k_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
@@ -117,10 +118,11 @@ class Attention(torch.nn.Module):
         self.out_proj = nn.Linear(self.num_attention_heads * self.head_dim, self.hidden_size, bias=False)
         self.q_norm = RMSNorm(self.head_dim, eps=config.rms_norm_eps)
         self.k_norm = RMSNorm(self.head_dim, eps=config.rms_norm_eps)
-        self.attn_droupout = nn.Dropout(config.dropout)
-        self.residual_droupout = nn.Dropout(config.dropout)
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.residual_dropout = nn.Dropout(config.dropout)
+        self.dropout = config.dropout
         self.flash = config.flash_attn and hasattr(torch.nn.functional, "scaled_dot_product_attention")
-    def forward(self, x, position_embeddings, past_kv = None, use_cache=False, attn_mask=None):
+    def forward(self, x, position_embeddings, past_key_value=None, use_cache=False, attn_mask=None):
         batch_size, seq_len, _ = x.size()
         xq, xk, xv = self.q_proj(x), self.k_proj(x), self.v_proj(x)
         xq = xq.view(batch_size, seq_len, self.num_attention_heads, self.head_dim)
@@ -130,27 +132,38 @@ class Attention(torch.nn.Module):
         xk = self.k_norm(xk)
         cos, sin = position_embeddings
         xq, xk = apply_rotary_pos_emb(xq, xk, cos, sin)
-        if past_kv is not None:
-            xk = torch.cat([past_kv[0], xk], dim=1)
-            xv = torch.cat([past_kv[1], xv], dim=1)
-        if use_cache:
-            past_kv = (xk, xv)
+        if past_key_value is not None:
+            xk = torch.cat([past_key_value[0], xk], dim=1)
+            xv = torch.cat([past_key_value[1], xv], dim=1)
+        past_kv = (xk, xv) if use_cache else None
         xq = xq.transpose(1, 2)
         xk = repeat_kv(xk, self.num_kv_repeats).transpose(1, 2)
         xv = repeat_kv(xv, self.num_kv_repeats).transpose(1, 2)
 
-        if self.flash and (seq_len > 1) and (not self.is_casual or past_kv is None):
-            attn_output = F.scaled_dot_product_attention(xq, xk, xv, attn_mask=attn_mask, dropout_p=self.attn_droupout if self.training else 0.0)
+        if self.flash and (seq_len > 1) and (not self.is_causal or past_key_value is None) and (attn_mask is None or torch.all(attn_mask == 1)):
+            attn_output = F.scaled_dot_product_attention(xq, xk, xv, dropout_p=self.dropout if self.training else 0.0, is_causal=self.is_causal)
         else:
             attn_scores = xq @ xk.transpose(-2, -1) / math.sqrt(self.head_dim)
-            if self.is_casual:
+            if self.is_causal:
                 attn_scores[:, :, :, -seq_len:] += torch.full((seq_len, seq_len), float('-inf'), device=attn_scores.device).triu(diagonal=1)
             if attn_mask is not None:
                 attn_scores += (1 - attn_mask.unsqueeze(1).unsqueeze(2)) * -1e9
-            attn_output = self.attn_droupout(F.softmax(attn_scores.float(), dim=-1)).type_as(xq) @ xv
+            attn_output = self.attn_dropout(F.softmax(attn_scores.float(), dim=-1)).type_as(xq) @ xv
         attn_output = attn_output.transpose(1, 2).reshape(batch_size, seq_len, -1)
-        attn_output = self.attn_droupout(self.out_proj(attn_output))
+        attn_output = self.residual_dropout(self.out_proj(attn_output))
         return attn_output, past_kv
+
+class FeedForward(nn.Module):
+    def __init__(self, config: ChaokaiMindConfig, intermediate_size: int = None):
+        super().__init__()
+        self.intermediate_size = intermediate_size or config.intermediate_size
+        self.up_proj = nn.Linear(config.hidden_size, self.intermediate_size, bias=False)
+        self.gate_proj = nn.Linear(config.hidden_size, self.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(self.intermediate_size, config.hidden_size, bias=False)
+        self.act_fn = ACT2FN[config.hidden_act]
+        self.dropout = nn.Dropout(config.dropout)
+    def forward(self, x):
+        return self.dropout(self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x)))
 
 
 
